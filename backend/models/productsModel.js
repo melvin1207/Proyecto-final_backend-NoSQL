24a737690af1d0a978c128f1f62f799a928cc7f8
Melvin Sanchez <melsava1207@gmail.com>
2024-02-12 10:37:07 -0600

    modelos hecho, primera edicion

--- backend/models/productsModel.js
+++ backend/models/productsModel.js
@@ -6,10 +6,30 @@ const productSchema = mongoose.Schema({
         required: true,
         ref: 'user'
     },
+    name:{
+        type: String,
+        required: [true, 'Por favor ingrese el nombre del producto']
+    },
     description:{
         type: String,
         required: [true, 'Por favor ingresa una descripción']
     },
+    cost:{
+        type: Number,
+        required: [true, 'Por favor ingresa cuanto cuesta el producto']
+    },
+    sku:{
+        type: String,
+        required: [true, 'Ingresa el SKU del producto']
+    },
+    category:{
+        type:String,
+        required: [true, 'Ingresa la categoria dle producto']
+    },
+    active:{
+        type: Boolean,
+        default: true
+    }
 }, {
     timestamps: true
 })
---
+++ backend/models/salesModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const saleSchema = mongoose.Schema({
+    sale: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    order_number:{
+        type: Number,
+        required: [true, 'Por favor ingrese el numero de orden']
+    },
+    product_quantity:{
+        type: Number,
+        required: [true, 'Por favor ingresa la cantidad del producto'], 
+    },
+    sales_price:{
+        type: Number,
+        required: [true, 'Ingresa el valor total de la venta']
+    },
+    active:{
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Sale', saleSchema)
