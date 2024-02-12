cddb903a35df8158a133e5e18ad46bbbe9e20ee7
Melvin Sanchez <melsava1207@gmail.com>
2024-02-12 10:44:40 -0600

    productsRoutes comenzado

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,2 +1,12 @@
 const express = require('express')
 const router = express.Router()
+
+const { protect } = require('../middleware/authMiddleware')
+
+router.get('/', protect, getProducts)
+router.post('/', protect, createProducts)
+
+router.options('/:id', protect, updateProducts)
+router.delete('/:id', protect, deleteProducts)
+
+module.exports = router
